468d70ddc7016d8553b7bc917bcd1cf1eebd61ac
roemhildtg <roemhildtg@gmail.com>
2014-12-11 12:40:31 -0600

    Fix undefined row in some relationship queries
    Not sure why it highlighted everything..change occurred on line 101

--- RelatedRecordTable_Widget/RelatedRecordTable.js
+++ RelatedRecordTable_Widget/RelatedRecordTable.js
@@ -1,11 +1,3 @@
-/*
- * 
- * RelatedRecordTable widget
- * Copyright (C) 2014 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 define([
     'dojo/_base/declare',
     'dijit/_WidgetBase',
@@ -24,8 +16,8 @@ define([
     'dojo/text!./RelatedRecordTable/templates/RelatedRecordTable.html',
     'dojo/domReady!'
 ], function (declare, _WidgetBase, _TemplatedMixin, _FloatingWidgetMixin, DomClass,
-        Array, Lang, Request, Memory, OnDemandGrid, ColumnHider, DijitRegistry,
-        TabContainer, css, tableTemplate) {
+        Array, Lang, Request, Memory, OnDemandGrid, ColumnHider,
+        DijitRegistry, TabContainer, css, tableTemplate) {
     var customGrid = declare('customGrid', [OnDemandGrid, ColumnHider, DijitRegistry]);
     return declare('RelatedRecordTable', [_WidgetBase, _TemplatedMixin, _FloatingWidgetMixin], {
         templateString: tableTemplate,
@@ -40,16 +32,16 @@ define([
         },
         postCreate: function () {
             this.inherited(arguments);
-            if(this.hasOwnProperty('parentWidget')){
+            if (this.hasOwnProperty('parentWidget')) {
                 DomClass.add(this.parentWidget.id, 'RelatedRecordsTableParent ' + this.tabPosition);
-            } 
-            
+            }
+
             this.tabContainer = new TabContainer({
                 tabPosition: this.tabPosition,
                 style: 'height:100%;width:100%;',
                 doLayout: false
             }, this.containerNode);
-            
+            var self = this;
             Array.forEach(this.layerInfos, Lang.hitch(this, function (layerInfo) {
                 if (layerInfo.layer.relationships && layerInfo.layer.relationships.length > 0) {
                     var relationshipLayer = {
@@ -79,10 +71,11 @@ define([
                             });
                             relationship.grid = new customGrid({
                                 title: relationship.title,
-                                store: relationship.store
+                                store: relationship.store,
+                                noDataMessage: 'Click a feature from the map to find related records'
                             });
                             relationshipLayer.relationships.push(relationship);
-                            this.tabContainer.addChild( relationship.grid );
+                            this.tabContainer.addChild(relationship.grid);
                         }
 
                     }));
@@ -93,7 +86,7 @@ define([
                 }
             }));
         },
-        onShow: function() {
+        onShow: function () {
             this.inherited(arguments);
             this.tabContainer.resize();
         },
@@ -105,7 +98,7 @@ define([
         _onLayerClick: function (layer, clickEvent) {
             Array.forEach(layer.relationships, Lang.hitch(this, function (relationship) {
                 if (relationship.include) {
-                    relationship.store.setData([{}]);
+                    relationship.store.setData([]);
                     relationship.grid.refresh();
                     var attributes = clickEvent.graphic.attributes;
                     var objectID = attributes[layer.layer.objectIdField];
