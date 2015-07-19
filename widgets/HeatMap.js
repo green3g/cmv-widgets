440085cd2fd5c7c25a7ecec2b765fe3311af7a95
roemhildtg <roemhildtg@gmail.com>
2015-07-18 19:23:22 -0500

    Add heatmap widget for additional example of modifying the menu icon.

---
+++ widgets/HeatMap.js
@@ -0,0 +1,57 @@
+define([
+    'dojo/_base/declare',
+    'dijit/_WidgetBase',
+    'dojo/topic',
+    'dojo/_base/lang',
+    'esri/layers/FeatureLayer',
+    'esri/renderers/HeatmapRenderer',
+    'dojo/dom-class'
+], function (declare, _WidgetBase, topic, lang, FeatureLayer, HeatmapRenderer, domClass) {
+    return declare([_WidgetBase], {
+        map: null,
+        cssClasses: ['fa', 'fa-fire'],
+        colors: [
+            'rgba(0,0,0,0.1)',
+            'rgba(0,0,255,0.7)',
+            'rgba(0,255,255,0.7)',
+            'rgba(0,255,0,0.7)',
+            'rgba(255,255,0,0.7)',
+            'rgba(255,0,0,0.7)'
+        ],
+        _heatMapLayers: {},
+        postCreate: function () {
+            this.inherited(arguments);
+            topic.subscribe('LayerControl/heatMap', lang.hitch(this, 'initHeatMap'));
+        },
+        initHeatMap: function (r) {
+            var layerId = r.layer.id + r.subLayer.id;
+            if (!this._heatMapLayers[r.layer.id]) {
+                this._heatMapLayers[r.layer.id] = {};
+            }
+            if (!this._heatMapLayers[layerId]) {
+                var serviceURL = r.layer.url + '/' + r.subLayer.id;
+                var heatmapFeatureLayerOptions = {
+                    mode: FeatureLayer.MODE_SNAPSHOT,
+                    outFields: ['*'],
+                    id: layerId
+                };
+                var heatmapRenderer = new HeatmapRenderer({
+                    colors: this.colors
+                });
+                this._heatMapLayers[layerId] = new FeatureLayer(serviceURL, heatmapFeatureLayerOptions);
+                this._heatMapLayers[layerId].setRenderer(heatmapRenderer);
+                this.map.addLayer(this._heatMapLayers[layerId]);
+            } else {
+                //toggle visibility
+                this._heatMapLayers[layerId].setVisibility(!this._heatMapLayers[layerId].visible);
+            }
+            if (r.iconNode) {
+                if (domClass.contains(r.iconNode, 'fa-fire')) {
+                    domClass.remove(r.iconNode, this.cssClasses);
+                } else {
+                    domClass.add(r.iconNode, this.cssClasses);
+                }
+            }
+        }
+    });
+});
