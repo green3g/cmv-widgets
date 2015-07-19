1e8bfd66bc9baf61c1ac7cedb7531e350b7c4a5a
roemhildtg <roemhildtg@gmail.com>
2015-07-18 19:22:28 -0500

    Add a simple metadata dialog widget. This demonstrates the menu item click functionality.

---
+++ widgets/MetadataDialog.js
@@ -0,0 +1,45 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dijit/_WidgetBase',
+    'dijit/Dialog',
+    'dijit/DialogUnderlay',
+    'dojo/topic',
+    'esri/request',
+    'dojo/text!./MetadataDialog/templates/dialogContent.html',
+    'xstyle/css!./MetadataDialog/css/MetadataDialog.css'
+], function (declare, lang, _WidgetBase, Dialog, DialogUnderlay, topic, request, dialogContent) {
+    return declare([_WidgetBase], {
+        dialog: null,
+        dialogTemplate: dialogContent,
+        postCreate: function () {
+            this.inherited(arguments);
+            topic.subscribe('LayerControl/showMetadata', lang.hitch(this, '_fetchMetadata'));
+        },
+        _fetchMetadata: function (event) {
+            new request({
+                url: event.layer.url + '/' + event.subLayer.id,
+                content: {
+                    f: 'json'
+                }
+            }).then(lang.hitch(this, '_showMetadata'));
+        },
+        _showMetadata: function (data) {
+            if (!this.dialog) {
+                this._createDialog(lang.replace(this.dialogTemplate, data));
+            } else {
+                this.dialog.set('content', lang.replace(this.dialogTemplate, data));
+            }
+            this.dialog.show();
+            DialogUnderlay.hide();
+        },
+        _createDialog: function (content) {
+            this.dialog = new Dialog({
+                title: 'Layer Metadata',
+                content: content || '',
+                style: 'width:400px;height:450px;',
+                open: false
+            });
+        }
+    });
+});
